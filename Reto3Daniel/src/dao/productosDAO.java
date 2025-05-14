5a07ea4557628ea96c4f51b4421f55bff3c0b740
danielchuperca <ciuperciucd51@gmail.com>
2025-05-14 11:59:29 +0200

    cambios en el main y productosDAO y productos y clientes

--- Reto3Daniel/src/dao/productosDAO.java
+++ Reto3Daniel/src/dao/productosDAO.java
@@ -37,4 +37,6 @@ public class productosDAO {
 		}
 		return pro;
 	}
+
+	
 }
--- Reto3Daniel/src/main/Main.java
+++ Reto3Daniel/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.util.Scanner;
 
 import clases.Clientes;
+import clases.Productos;
 import dao.ClientesDAO;
 
 public class Main {
@@ -31,10 +32,34 @@ public class Main {
 				ClientesDAO.inserta(clientes);
 				System.out.println("el cliente se ha insertado correctamente");
 			case 2:
-			
+				int cod=util.Funciones.dimeEntero("dime el codigo que quieres que busque", sc);
+				if(dao.ClientesDAO.buscarCodigo(cod)!=null) {
+					System.out.println(dao.ClientesDAO.buscarCodigo(cod)); 
+					System.out.println("añade los datos necesarios");
+					String nombre2=util.Funciones.dimeString("dime el nombre del cliente", sc);
+					String direccion2=util.Funciones.dimeString("dime la direccion del cliente", sc);
+					int codigo2=util.Funciones.dimeEntero("dime el codigo del cliente", sc);
+					Clientes clientes2=new Clientes(nombre2,direccion2,codigo2);
+					dao.ClientesDAO.actualizarCliente(clientes2,cod);
+					System.out.println("se ha actualizado correctamente");
+				}
+				else {
+					System.out.println("no existe");
+				}
+				
 			}
 		case 2:
-
+				
+				String nombre=util.Funciones.dimeString("dime el nombre del producto que quieres", sc);
+				String talla=util.Funciones.dimeString("dime la talla del producto que quieres", sc);
+				String color=util.Funciones.dimeString("dime el color que quieres del producto", sc);
+				Productos prod=new Productos(nombre,talla,color);
+				if(prod.getNombre().equals(nombre)||prod.getTalla().equals(talla)||prod.getColor().equals(color)) {
+				System.out.println(dao.productosDAO.buscarProdu(prod)); 
+				}
+				else {
+				System.out.println(dao.productosDAO.buscarProdu(prod));
+				}
 		case 3:
 		
 		case 4:
